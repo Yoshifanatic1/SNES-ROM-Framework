48a3227fece112a5b13c5215464cc16f3e04b391
Yoshifanatic1 <60048201+Yoshifanatic1@users.noreply.github.com>
2021-08-14 17:24:31 -0400

    Add New Useful Scripts
    One for setting up the asset extraction table for that script and one for making it easier to set up relative pointer tables.

---
+++ Extras/SetupAssetExtractionTable.asm
@@ -0,0 +1,31 @@
+; This will dump the data for an asar patch that will be applied to any ROM. Said patch will set up the asset extraction script data entries when applied to the ROM.
+; The reason I'm generating a patch and not the tables directly is because of asar limitations. I don't think it's possible for asar to resolve commands through a define while in a print statement.
+; Also, it may take a second before asar starts displaying anything on the command line. In addition, you'll need to replace the ' with " in the output patch, and the % with " in the second output patch, otherwise asar won't assemble the patch.
+
+!PtrStartOffset = $EE0D86
+!PtrEndOffset =	$EE1014
+
+hirom
+
+!LoopCounter1 = 0
+!LoopCounter2 = 0
+print "hirom"
+
+while !PtrStartOffset+!LoopCounter1 < !PtrEndOffset
+	!Input1 #= read3(!PtrStartOffset+!LoopCounter1)
+	!Input2 #= read3(!PtrStartOffset+!LoopCounter1+$03)
+	print "print '	dl $',hex(!Input1+$04),',$',hex(!Input2),',BRRFile',hex(!LoopCounter2, 2),',BRRFile',hex(!LoopCounter2, 2),'End'"
+	!LoopCounter1 #= !LoopCounter1+3
+	!LoopCounter2 #= !LoopCounter2+1
+endif
+
+!LoopCounter1 = 0
+!LoopCounter2 = 0
+
+while !PtrStartOffset+!LoopCounter1 < !PtrEndOffset
+	print "print 'BRRFile',hex(!LoopCounter2, 2),':'"
+	print "print '	db %',hex(!LoopCounter2, 2),'.brr%'"
+	print "print 'BRRFile',hex(!LoopCounter2, 2),'End:'"
+	!LoopCounter1 #= !LoopCounter1+3
+	!LoopCounter2 #= !LoopCounter2+1
+endif
---
+++ Extras/SetupRelativePointerTable.asm
@@ -0,0 +1,20 @@
+@asar 1.81
+; This will create a pointer table that uses relative offsets when applied to the ROM. Just set the beginning and end offset of the pointer table.
+
+hirom
+!LoopCounter = 0
+!Offset = $FD934D
+!EndOffset = $FD94ED
+while !LoopCounter < !EndOffset-!Offset
+	!Input1 = read2(!Offset+!LoopCounter)
+	print "	dw DATA_",hex(!Input1+!Offset, 6),"-DATA_",hex(!Offset, 6)
+	!LoopCounter #= !LoopCounter+2
+endif
+
+print ""
+!LoopCounter #= 0
+while !LoopCounter < !EndOffset-!Offset
+	!Input1 = read2(!Offset+!LoopCounter)
+	print "DATA_",hex(!Input1+!Offset, 6),":"
+	!LoopCounter #= !LoopCounter+2
+endif
