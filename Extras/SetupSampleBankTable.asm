8b3b991cff84a868ab5367123c668c3e4833f0df
Yoshifanatic1 <60048201+Yoshifanatic1@users.noreply.github.com>
2021-09-29 22:46:30 -0400

    Add Asset Extraction Pointer Table Setup

--- Extras/SetupSampleBankTable.asm
+++ Extras/SetupSampleBankTable.asm
@@ -1,10 +1,14 @@
 @asar 1.81
 ; This will set up the sample pointer table and data when applied to the ROM. Just set the beginning and end offset of the pointer table.
 
+hirom
+
 !LoopCounter = 0
-!SubFolder = "FlySwatting/"
-!Offset = $1E9FAC
-!EndOffset = $1EA02C
+!SubFolder = ""
+!Offset = $C219AE
+!EndOffset = $C21A4E
+!StartOfSampleData = $C21A52
+!BaseAddressOfSampleData = $6F00
 while !LoopCounter < !EndOffset-!Offset
 	!Input1 = read2(!Offset+!LoopCounter)
 	!Input2 = read2(!Offset+!LoopCounter+$02)-!Input1
@@ -31,3 +35,20 @@ while !LoopCounter < !EndOffset-!Offset
 	endif
 	!LoopCounter #= !LoopCounter+4
 endif
+
+print ""
+!LoopCounter #= 0
+while !LoopCounter < !EndOffset-!Offset-$04
+	!Input1 = read2(!Offset+!LoopCounter)
+	!Input2 = read2(!Offset+!LoopCounter+$04)
+	if !Input1 == $FFFF
+	elseif !Input1 == $0000
+	elseif !Input1 > !Input2
+		warn "The pointers in this table aren't stored in order. You may want to sort the pointer from lowest to highest address, and then re-apply this script."
+		!LoopCounter #= !EndOffset-!Offset
+	else
+		print "	dl $",hex(!StartOfSampleData+(!Input1-!BaseAddressOfSampleData), 6),",$",hex(!StartOfSampleData+(!Input2-!BaseAddressOfSampleData), 6),",BRRFile",hex(!LoopCounter/$04, 2),",BRRFile",hex(!LoopCounter/$04, 2),"End"
+	endif
+	!LoopCounter #= !LoopCounter+4
+endif
+	print "Note: You may need to set up the last entry in the asset extraction table manually."
